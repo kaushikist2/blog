630a056697cb99f9d23d6084b19259f15db57222
kaushiki t <kaushikit058@gmail.com>
2026-01-30 11:14:43 +0530

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Playfair Display", "serif"],
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
